062ad5324d8a5ecdb52652e4f7035bf5c39ed513
Susie Chen <suujia@icloud.com>
2018-07-31 16:55:59 -0700

    lol

--- main.go
+++ main.go
@@ -1,81 +1,99 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"net/url"
 )
 
-type todo struct {
-	// set as true when edit or add button is pressed
-	EnableEdit bool   `false`
-	Id         string `json:"id"`
-	Title      string `json:"title"`
-	Completed  bool   `json:"completed"`
-	Action     string `ADD`
+type Event struct {
+	Id        string `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
 }
+
+type Todo struct {
+	Id        string `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+type Person struct {
+	Id        string   `json:"id"`
+	Firstname string   `json:"firstname"`
+	Lastname  string   `json:"lastname"`
+	Address   *Address `json:"address, omitempty"`
+}
+
+type Address struct {
+	City    string `json:"city, omitempty"`
+	Country string `json:"country, omitempty"`
+}
+
+var people []Person
+var events []Event
+var eventId int
 
 func main() {
-	do := &todo{EnableEdit: false, Id: "123", Title: "Cultus Lake", Completed: false, Action: "ADD"}
-	http.HandleFunc("/", handle)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", GetPeople)
+	http.HandleFunc("/{id}", handle)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
+	events := append(events, Event{Id: "122", Title: "Las Vegas", Completed: true})
+	events = append(events, Event{Id: "123", Title: "Cultus Lake", Completed: false})
+
+	people := append(people, Person{Id: "1", Firstname: "Susie", Lastname: "Chen", Address: &Address{City: "Vancouver", Country: "Canada"}})
+	people = append(people, Person{Id: "2", Firstname: "Dylan", Lastname: "Whitney"})
 }
 
-func handle(response http.ResponseWriter, request *http.Request) {
-	switch request.Method {
+// fetch all from people slice
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	// display one person's data
 	case "GET":
-		response, err := http.Get("https://api.coinbase.com/v2/prices/BTC-USD/buy")
-		if err != nil {
-			fmt.Printf("HTTP request failed with error %s\n", err)
-		} else {
-			data, err := ioutil.ReadAll(response.Body)
-			response.WriteHeader(http.StatusOK)
-			fmt.Println(string(data))
-		}
-	case "ADD":
-		if do.EnableEdit == true {
-			response, err = http.Put("https://httpbin.org/put", todoIndex, this.todo)
-			if err != nil {
-				fmt.Printf("HTTP request failed with error %s\n", err)
-			} else {
-				err := request.ParseForm()
-
-				this.todos[this.jsonData.todoIndex] = this.todo
-				this.enableEdit = false
-				data, err := ioutil.ReadAll(response.Body)
-				response.Header().Set("Content-Type", "text/html")
+		for _, ev := range people {
+			if ev.Id == r.FormValue("Id") {
+				json.NewEncoder(w).Encode(ev)
 				w.WriteHeader(http.StatusOK)
-				fmt.Printf(response, string(data))
+				return
 			}
 		}
-	case "DELETE":
-		if confirm("Are you sure?") {
-			response, err := http.Delete("https://httpbin.org/delete")
 
-			if err != nil {
-				fmt.Printf("HTTP request failed with error %s\n", err)
-			} else {
-				// this.http.Delete(todo).then(response => {
-				//     if(response.status == 200){
-				//         this.todos.splice(todoIndex, 1)
-				// }
-				response.WriteHeader(http.StatusOK)
-				fmt.Println("you're deleting this todo")
-			}
-		}
-	case "POST":
-		resp, err := http.PostForm("https://httpbin.org/form",
-			url.Values{"key": {"Value"}, "id": {"123"}})
+	case "ADD":
+		var event Event
+
+		event.Id = r.FormValue("Id")
+		event.Title = r.FormValue("Title")
+		event.Completed = false
+		events = append(events, event)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(events))
 		w.WriteHeader(http.StatusOK)
-		fmt.Println("you're editing this todo")
+
+	case "DELETE":
+		for index, ev := range events {
+			if ev.Id == r.FormValue("Id") {
+				people = append(people[:index], people[index+1:]...)
+				break
+			}
+			w.WriteHeader(http.StatusOK)
+			fmt.Println("you're deleting this todo")
+		}
 
 	case "PATCH":
+		// complete todos, change title
 		w.WriteHeader(http.StatusOK)
-		fmt.Println("todo completed!")
+		fmt.Println("you're editing this todo!")
 
 	default:
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write("unrecognized request")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("default action!")
 	}
 }
